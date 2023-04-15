2be5a065b7988cf7f87a7b3b7471ba33459ba16c
klaus <124338898+xxxiklaus@users.noreply.github.com>
2023-04-15 18:07:02 +0800

    todo

---
+++ pac-man/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	// initialize game
+
+	// load resources
+
+	// game loop
+	for {
+		// update screen
+
+		// process input
+
+		// process movement
+
+		// process collisions
+
+		// check game over
+
+		// Temp: break infinite loop
+		fmt.Println("Hello Pac-man Go!")
+		break
+
+		// repeat
+	}
+}
